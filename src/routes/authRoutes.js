ae41794ab42723fd069dd6986f2eee35c76a6e8d
Mykyta <angertina777@ukr.net>
2026-08-06 23:56:00 +0300

    create login router and login validation

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 // import { celebrate } from 'celebrate';
 import { refreshUserSession } from '../controllers/authController.js';
+import { celebrate } from 'celebrate';
+import { loginSchame } from '../validations/authValidation.js';
 
 const router = Router();
 
+router.post('/auth/login', celebrate(loginSchame));
 router.post('/auth/refresh', refreshUserSession);
 
 export default router;
