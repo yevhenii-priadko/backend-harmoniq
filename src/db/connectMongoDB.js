d449bc2d0dc22a1645f97a731160a0e9c8a47820
Lis <genia.grishenko@gmail.com>
2026-08-06 12:28:14 +0300

    Repository configurations

---
+++ src/db/connectMongoDB.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+// import { Article } from '../models/article.js';
+
+export const connectMongoDB = async () => {
+  try {
+    const mongoUrl = process.env.MONGO_URL;
+    await mongoose.connect(mongoUrl);
+    console.log('✅ MongoDB connection established successfully');
+    // await Article.syncIndexes();
+    // console.log('Indexes synced successfully');
+  } catch (error) {
+    console.error('❌ Failed to connect to MongoDB:', error.message);
+    process.exit(1);
+  }
+};
---
+++ src/middleware/multer.js
@@ -0,0 +1,15 @@
+import multer from 'multer';
+
+export const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 2 * 1024 * 1024,
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith('image/')) {
+      cb(null, true);
+    } else {
+      cb(new Error('Only images allowed'), false);
+    }
+  },
+});
