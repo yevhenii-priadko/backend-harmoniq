6d62607860a4f5c53362061bdefc06f9bad0c2c5
Ok_sana <sansen3000sansen@gmail.com>
2026-08-08 19:39:35 +0300

    Add logout endpoint

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,16 +2,19 @@ import { Router } from 'express';
 import { celebrate } from 'celebrate';
 import {
   loginUser,
+  logoutUser,
   refreshUserSession,
   registerUserController,
 } from '../controllers/authController.js';
 import { loginSchame } from '../validations/authValidation.js';
 import { registerUserValidation } from '../validations/authValidation.js';
+import { authenticate } from '../middleware/authenticate.js';
 
 const router = Router();
 
 router.post('/auth/register', registerUserValidation, registerUserController);
 router.post('/auth/login', celebrate(loginSchame), loginUser);
+router.post('/auth/logout', authenticate, logoutUser);
 router.post('/auth/refresh', refreshUserSession);
 
 export default router;
