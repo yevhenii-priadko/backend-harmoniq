c2f3e7dd9a8be3dba0ad7323c93941bd15444d62
Oleksandra Starovatova <starovatovasasha@gmail.com>
2026-08-09 08:19:30 +0300

    deleteArticleById

--- src/controllers/articlesController.js
+++ src/controllers/articlesController.js
@@ -30,6 +30,29 @@ export const getArticleById = async (req, res) => {
   res.status(200).json(article);
 };
 
+export const deleteArticleById = async (req, res) => {
+  const { articleId } = req.params;
+  const userId = req.user._id;
+  
+  const article = await Article.findOne({
+    _id: articleId,
+  });
+  if (!article) {
+    throw createHttpError(404, 'Article not found');
+  }
+  if (article.userId.toString() !== userId.toString()) {
+    throw createHttpError(403, 'No rights to delete the article');
+  }
+
+  await Article.findOneAndDelete({
+    _id: articleId
+  });
+
+  res.status(200).json({
+    message: "Article is deleted successfully"
+  });
+};
+
 export const createArticle = async (req, res) => {
   const article = await Article.create({
     ...req.body,
