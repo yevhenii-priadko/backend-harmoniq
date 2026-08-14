0cb46c242afbd45e3d72716ad469efd2b2b67cf0
Ign1s123 <genamisinskij@gmail.com>
2026-08-14 11:56:51 +0300

    fix: update author field validation to allow shorter names

--- src/validations/articlesValidation.js
+++ src/validations/articlesValidation.js
@@ -18,7 +18,7 @@ export const createArticleSchema = {
     date: Joi.string()
       .pattern(/^\d{4}-\d{2}-\d{2}$/)
       .required(),
-    author: Joi.string().min(4).max(50).required(),
+    author: Joi.string().min(2).max(50).required(),
   }),
 };
 
